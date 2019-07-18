12c13b8569d0ed0ef34ad43e51eda949f6a694e6
Eduard Panin <epanin@ascendix.com>
2019-07-18 11:01:11 +0300

    Singleton

--- js/task2.js
+++ js/task2.js
@@ -1,22 +1,22 @@
 function Vehicle(){
     let speed = 0;
-    this.speedUp = function(){
+    this.speedUp = function() {
         speed++;
         showSpeed();
     };
 
-    this.applyBrake = function(){
+    this.applyBrake = function() {
         if (speed > 0) {
             speed--;
         }
         showSpeed();
     };
 
-    let showSpeed = function () {
+    let showSpeed = function() {
         console.log(speed);
     };
 
-    this.monitor = function(){
+    this.monitor = function() { //Arrow function here?
         console.log(this.type);
     };
 
@@ -48,7 +48,29 @@ function Car() {
 }
 Car.prototype = new Vehicle();
 
+let Garage = (function () {
+	let instance;
+
+	return function Construct_garage () {
+		if (instance) {
+			return instance;
+		}
+		if (this && this.constructor === Construct_garage) {
+			instance = this;
+		} else {
+			return new Construct_garage();
+		}
+	}
+}());
+
 function testLogic() {
-    let car = new Segway();
-    car.monitor();
+	let garage = new Garage();
+	garage.vehicles = [new Segway(), new Bike(), new MountainBike(), new HorseCarriage(), new Car()];
+	garage = new Garage();
+	garage.vehicles.forEach(vehicle => {
+		vehicle.monitor();
+		vehicle.speedUp();
+		vehicle.speedUp();
+		vehicle.applyBrake();
+	});
 }
